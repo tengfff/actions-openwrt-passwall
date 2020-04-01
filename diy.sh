207209831eb7e2cf9e1771609eb267b0c706594e
tengfff <59329194+tengfff@users.noreply.github.com>
2020-04-01 10:08:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: tengfff
+# Blog: https://flyflyt.xyz
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
